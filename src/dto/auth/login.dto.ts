5becdb8ef3ed7df61bdfd894aca3effccc474a2e
Tahir Sixlarov <sixlarovtahir13@gmail.com>
2025-11-25 00:49:50 +0400

    fix_login_re

--- src/dto/auth/login.dto.ts
+++ src/dto/auth/login.dto.ts
@@ -4,5 +4,5 @@ export const LoginDto = z.object({
   email: z.string().email({ message: "Invalid email format" }),
   password: z
     .string()
-    .min(8, { message: "Password must be at least 8 character" }),
+    .min(8, { message: "Password must be at least 8 characters" }),
 });
--- src/dto/auth/register.dto.ts
+++ src/dto/auth/register.dto.ts
@@ -1,13 +1,13 @@
 import { z } from "zod";
 export const RegisterDto = z.object({
-  email: z.string().email({message:"Invalid email format"}),
+  email: z.string().email({ message: "Invalid email format" }),
   password: z
     .string()
-    .min(8, "Password must be at least 8 characters")
-    .regex(/[A-Z]/, "Password must be contain uppercase")
-    .regex(/[a-z]/, "Password must contain lowercase")
-    .regex(/[0-9]/, "Password must contain number"),
-  name: z.string().min(2, "Name must be at least 2 characters"),
-  roleId: z.string().uuid({message:"Invalid role ID"}),
+    .min(8, { message: "Password must be at least 8 characters" })
+    .regex(/[A-Z]/, { message: "Password must contain at least one uppercase letter" })
+    .regex(/[a-z]/, { message: "Password must contain at least one lowercase letter" })
+    .regex(/[0-9]/, { message: "Password must contain at least one number" }),
+  name: z.string().min(2, { message: "Name must be at least 2 characters" }),
+  roleId: z.string().uuid({ message: "Invalid role ID" }),
 });
 
